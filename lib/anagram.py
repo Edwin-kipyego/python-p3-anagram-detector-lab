ea16bb72215ca7f80b250c79939661dfbeee7a5c
Edwin kipyego <koriredwin95@gmail.com>
2025-05-13 00:17:21 +0300

    complete anagram detector lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
-# your code goes here!
+
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        original_sorted = sorted(self.word)
+        matches = []
+
+        for word in word_list:
+            if word.lower() != self.word and sorted(word.lower()) == original_sorted:
+                matches.append(word)
+
+        return matches
